e95db908f2b407b7248cf9ae185753f9f704948f
Abraim-ng <abraimmostapha@yahoo.com>
2022-10-18 10:51:23 +0100

    style

---
+++ script.js
@@ -0,0 +1,30 @@
+// Dom Manipulation 
+
+
+
+// GetElementById()
+
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+// GetElementByClassName()
+// const listItems =  document.getElementsByClassName('list-items')
+// console.log(listItems)
+
+
+// GetElementByTagName()
+
+// const listItems =  document.getElementsByTagName('li')
+// console.log(listItems)
+
+// querySelector()
+// const container = document.querySelector('div');
+// console.log(container);
+
+// querySelectorAll()
+
+const container = document.querySelectorAll('div');
+
+console.log(container);
+
+
