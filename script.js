3d6b46e36322d5c6169e1210f693943d4dc06fce
Abraim-ng <abraimmostapha@yahoo.com>
2022-10-18 15:48:10 +0100

    traverse

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-// Dom Manipulation 
-
-
-
-// GetElementById()
-
-// const title = document.getElementById('main-heading');
+// Dom Manipulation
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'red'; //change the color of the main heading
 // console.log(title);
 
-// GetElementByClassName()
-// const listItems =  document.getElementsByClassName('list-items')
-// console.log(listItems)
+
+// ------------------Styling Elements ----------------------------------
+// const listItems = document.querySelectorAll('.list-items');
+
+// for ( i = 0; i < listItems.length; i++ ) {
+//     listItems[i].style.fontSize = '2rem';
+// }
+
+// console.log(listItems);
+
+//creating elements
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
 
 
-// GetElementByTagName()
+// adding elements
 
-// const listItems =  document.getElementsByTagName('li')
-// console.log(listItems)
+// ul.append(li); //add text
 
-// querySelector()
-// const container = document.querySelector('div');
-// console.log(container);
+// modifying the text
 
-// querySelectorAll()
+// li.innerText = 'X-Men';
 
-const container = document.querySelectorAll('div');
+// Modifying Attributes & Classes
 
-console.log(container);
+// li.classList.add('list-items')
 
 
+
+
+
+
+
+// Dom Manipulation
+
+// Traverse the DOM
+
+// Parent Node Traversal
+
+// Child Node Traversal
+// let ul = document.querySelector('ul');
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+
+// ul.childNodes[1].style.backgroundColor = 'blue';
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+// Sibling Node Traversal
+
+let ul = document.querySelector('ul');
+const div = document.querySelector('div');
+console.log(div.childNodes);    
+console.log(ul.previousSibling);
+console.log(ul.nextSibling);
